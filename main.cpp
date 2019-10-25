a8c74efd7214d8889c727aa59d5865f59313839e
John Weachock <jweachock@gmail.com>
2019-10-25 19:06:03 +0000

    Add cocktail buttons

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@
 
 #define DEBOUNCE 30
 #define NUM_PUMPS 6
+#define NUM_BUTTS 4
+#define MIN_CUP_WEIGHT 5
 
 class Knob {
 private:
@@ -46,12 +48,12 @@ private:
 
 public:
   Button(int pin)
-      : pin(pin), lastDebounce(0), buttonState(LOW), lastButtonState(HIGH) {}
+      : pin(pin), lastDebounce(0), buttonState(HIGH), lastButtonState(HIGH) {}
 
-  void init() { pinMode(this->pin, INPUT); }
+  void init() { pinMode(this->pin, INPUT_PULLUP); }
 
   /// this does *not* do any debouncing!
-  bool read() { return digitalRead(this->pin); }
+  bool read() { return !digitalRead(this->pin); }
 
   /// This returns true if the button state CHANGED. It does *not* return the
   /// button state! If you want to react to presses, use
@@ -126,24 +128,108 @@ Pump pumps[NUM_PUMPS] = {
     Pump(4), Pump(5), Pump(6), Pump(7), Pump(8), Pump(9),
 };
 
+Button butts[NUM_BUTTS] = {
+  Button(A2), Button(A3), Button(A4), Button(A5),
+};
+
+Beeper beeper(10);
+
 Scale scale(3, 2, -13500);
 
-void press(int i) {}
+void beep_err() {
+  beeper.beep(400);
+}
+
+void beep_succ() {
+  beeper.beep(100);
+  delay(200);
+  beeper.beep(100);
+}
+
+void press(int i) {
+  float cupWeight = scale.read();
+  Serial.print("cupWeight = ");
+  Serial.println(cupWeight);
+
+  if(cupWeight < MIN_CUP_WEIGHT) {
+    Serial.println("NO CUP");
+    beep_err();
+    return;
+  }
+
+  beep_succ();
+
+  int startTime = millis();
+
+  if(i == 0) {
+    pumps[0].turnOn();
+    delay(1000);
+    pumps[0].turnOff();
+  }
+  else if(i == 1) {
+    pumps[0].turnOn();
+    delay(5000);
+    pumps[0].turnOff();
+  }
+  else if(i == 2) {
+    pumps[5].turnOn();
+    delay(1000);
+    pumps[5].turnOff();
+  }
+  else if(i == 3) {
+    pumps[5].turnOn();
+    delay(5000);
+    pumps[5].turnOff();
+  }
+
+  int endTime = millis();
+
+  delay(5000);
+  beep_succ();
+
+  float finalWeight = scale.read();
+  Serial.print("finalWeight = ");
+  Serial.println(finalWeight);
+
+  float drinkWeight = finalWeight - cupWeight;
+  Serial.print("drinkWeight = ");
+  Serial.println(drinkWeight);
+
+  float pourTime = (endTime - startTime) / 1000.0;
+  Serial.print("pourTime = ");
+  Serial.print(pourTime);
+  Serial.println(" sec");
+
+  float pourSpeed = drinkWeight / pourTime;
+  Serial.print("pourSpeed = ");
+  Serial.print(pourSpeed);
+  Serial.println(" per sec");
+
+  // dispense(0, 2);
+  // dispense(5, 6);
+  // dispense(2, 6);
+}
 
 void dispense(int pump, float amt) {
-  int delayFactor = 600;
-  int quit = 100;
+  int delayFactor = 800;
+  int quit = 200;
   int leadTime = 200;
   int lagTime = 3000;
-  float dropRate = 0.5;
+  float dropRate = 0.9;
 
   float cur = scale.read();
   float target = cur + amt;
 
   while(true) {
+    Serial.print("@ ");
+    Serial.print(cur);
+    Serial.print(" / ");
+    Serial.println(target);
+
     int delayTime = (target - cur) * delayFactor;
 
     if(delayTime <= quit || cur > target) {
+      Serial.println("pump decay is too low");
       break;
     }
 
@@ -163,6 +249,7 @@ void setup() {
   Serial.println("booting...");
 
   scale.init();
+  beeper.init();
 
   for (int i = 0; i < NUM_PUMPS; i++) {
     pumps[i].init();
@@ -171,32 +258,26 @@ void setup() {
     Serial.println(" ready.");
   }
 
+  for (int i=0; i<NUM_BUTTS; i++) {
+    butts[i].init();
+  }
+
   delay(3000);
 
   scale.tare();
 
   Serial.println("booted.");
-  Serial.println("place cup now.");
-  delay(5000);
 
-  float cup = scale.read();
-  Serial.print("cup weighs ");
-  Serial.println(cup);
-
-  if(cup < 5) {
-    Serial.println("NO CUP");
-    return;
-  }
-
-  dispense(0, 4);
-  dispense(5, 4);
+  beep_succ();
 }
 
 void loop() {
-  Serial.print(millis(), DEC);
-  Serial.print("\t");
-  Serial.println(scale.read());
-  delay(500);
+  for(int i=0; i<NUM_BUTTS; i++) {
+    if(butts[i].checkPress() && butts[i].isPressed()) {
+      press(i);
+    }
+  }
+  delay(17);
 }
 
 int main(void) {
